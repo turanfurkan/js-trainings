43b1cd266ca12f4c277feffb31fcf16ad9e88808
Furkanturanr <furkanturan10r@hotmail.com>
2024-11-12 19:46:41 +0300

    An example related to Map was made

---
+++ keyed-collections/map/script.js
@@ -0,0 +1,11 @@
+//Student Grades with Map
+
+const students = new Map();
+students.set("Furkan", 35);
+students.set("Ahmet", 12);
+students.set("Kemal", 5);
+students.set("Kadir", 96);
+
+students.forEach((grade, name) => {
+    console.log(`${name}'s grade: ${grade}`);
+});
